24076cb4d45830bd4376f02e4b3384421b8a7607
Sagar Newpane <sdreamnewpane@gmail.com>
2025-02-25 21:26:40 +0545

    made it to send the otp through mail

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -187,8 +187,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # or your email host
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
